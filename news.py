fc3c03d248561b87797c05d8cbc07b52043a3ad7
Arthur <z123483245x@gmail.com>
2023-05-03 16:14:18 +0800

    20230503

--- news.py
+++ news.py
@@ -1,32 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-# from datetime import datetime, date
-
-# url = 'https://news.ltn.com.tw/list/breakingnews/business'
-
-# res = requests.get(url)
-# soup = BeautifulSoup(res.text, 'html.parser')
-
-# news = soup.select('ul.list > li')
-
-# for item in news:
-#     # 获取新闻标题和時間
-#     title_and_time = item.select_one('a.tit').text.strip()
-#     time_str = title_and_time.split()[0]
-#     title = ' '.join(title_and_time.split()[1:])
-
-#     # 获取新闻链接
-#     link = item.select_one('a.tit')['href']
-
-#     # 解析新闻时间
-#     hour, minute = time_str.split(':')
-#     time = datetime.strptime(time_str, '%H:%M')
-#     today = date.today()
-#     datetime_obj = datetime.combine(today, time.time())
-#     formatted_date = datetime_obj.strftime('%Y-%m-%d')
-
-#     # 输出结果
-#     print(f'Title: {title}\nLink: {link}\nTime: {hour}:{minute}\nDate: {formatted_date}\n')
 
 import csv
 import requests
